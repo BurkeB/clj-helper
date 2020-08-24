03241bd796d4bf72a37eb2c380cd282dd6026021
Bruno Burke <burke@fh-muenster.de>
2020-08-24 18:12:57 +0200

    change version to 0.0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wwsoftware/clj-helper "0.0.3.2"
+(defproject wwsoftware/clj-helper "0.0.3.3"
   :description "Helper Functions for Clojure(-Script) Projects"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
