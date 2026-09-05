f443f569687bc82cd69999682a74823b0fcc3917
Bruno Burke <burke@fh-muenster.de>
2026-09-05 10:11:33 +0200

    add spdx headers

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject wwsoftware/clj-helper "0.0.3.13"
+(defproject wwsoftware/clj-helper "0.0.3.14"
   :description "Helper Functions for Clojure(-Script) Projects"
   :url "https://git.fh-muenster.de/ww-software/libraries/clj-helper"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Eclipse Public License - v 2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.clj-commons/digest "1.4.100"]
--- src/clj_helper/edn.cljc
+++ src/clj_helper/edn.cljc
@@ -1,3 +1,12 @@
+;; Copyright © 2020-2026 FH Münster and contributors
+;; Author: Bruno Burke <burke@fh-muenster.de>
+;;
+;; This program and the accompanying materials are made available under the
+;; terms of the Eclipse Public License 2.0 which is available at
+;; https://www.eclipse.org/legal/epl-2.0/
+;;
+;; SPDX-License-Identifier: EPL-2.0
+
 (ns clj-helper.edn
   (:require [clojure.string :as string]
             #?(:clj [clojure.edn :as edn]
